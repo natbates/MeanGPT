ac98db90fdfb26c576c1fc45b0b8f68730d87318
Nathaniel Bates <nathaniel.christopher.bates@gmail.com>
2025-09-23 21:27:25 +0100

    fix router dom issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ChatHistory from "./chat-components/ChatHistory";
 import { ChatContext } from "../contexts/ChatContext";
